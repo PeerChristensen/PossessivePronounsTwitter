8b93474bfe69f61c16c9dada6bed59940890867b
PeerChristensen <hr.pchristensen@gmail.com>
2018-12-16 09:43:17 +0100

    4th

--- her_bigrams.R
+++ her_bigrams.R
@@ -29,11 +29,31 @@ her <- her %>%
 herCount <- her %>%
   count(word1,word2) %>%
   arrange(desc(n)) %>%
-  select(word2,n)
+  select(word2,n) %>%
+  mutate(row = rev(row_number()))
+
 #unite(bigram,word1,word2, sep = " ")
 
 wordcloud(words = herCount$word2, freq = herCount$n, min.freq = 50, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(9,"Reds")[4:9])
+
+herCount %>%
+  top_n(20,n) %>%
+  ggplot(aes(row, n, fill = n)) +
+  geom_col(show.legend = FALSE,width=.9) +
+  coord_flip() +
+  scale_x_continuous( 
+    breaks = herCount$row,
+    labels = herCount$word2) +
+  theme_minimal() + 
+  theme(axis.text  = element_text(size = 16),
+        axis.title   = element_text(size = 18),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        #axis.title.y = element_text(margin = margin(r = 40,l=40)),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank()) +
+  scale_fill_gradient(low=brewer.pal(9,"Reds")[2],high=brewer.pal(9,"Reds")[9])
 
 
   
--- his_bigrams.R
+++ his_bigrams.R
@@ -26,9 +26,30 @@ his <- his %>%
 hisCount <- his %>%
   count(word1,word2) %>%
   arrange(desc(n)) %>%
-  select(word2,n)
+  select(word2,n) %>%
+  mutate(row = rev(row_number()))
   #unite(bigram,word1,word2, sep = " ")
 
 wordcloud(words = hisCount$word2, freq = hisCount$n, min.freq = 50, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(9,"Blues")[4:9])
+
+
+hisCount %>%
+  top_n(20,n) %>%
+  ggplot(aes(row, n, fill = n)) +
+  geom_col(show.legend = FALSE,width=.9) +
+  coord_flip() +
+  scale_x_continuous( 
+    breaks = hisCount$row,
+    labels = hisCount$word2) +
+  theme_minimal() + 
+  theme(axis.text  = element_text(size = 16),
+        axis.title   = element_text(size = 18),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        #axis.title.y = element_text(margin = margin(r = 40,l=40)),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank()) +
+  scale_fill_gradient(low=brewer.pal(9,"Blues")[2],high=brewer.pal(9,"Blues")[9])
+
 
