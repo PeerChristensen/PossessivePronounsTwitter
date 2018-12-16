e94525e12ee931fd43d8979fee979f30685f601b
PeerChristensen <hr.pchristensen@gmail.com>
2018-12-16 09:24:02 +0100

    3rd

--- her_bigrams.R
+++ her_bigrams.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(tidytext)
 library(cldr)
+library(udpipe)
 
 her <- read_csv("herTweets.csv") %>%
   select(screen_name, text) %>% 
@@ -12,6 +13,27 @@ her <- her %>%
   separate(bigram, c("word1", "word2"), sep = " ") %>%
   filter(word1 == "her")
 
-her %>% count(word1,word2) %>%
+#udmodel <- udpipe_download_model(language = "english")
+
+include <- udpipe(x = her$word2,
+             object = udmodel)
+
+include <- include %>%
+  select(token,upos) %>%
+  filter(upos =="NOUN") %>%
+  select(token) 
+
+her <- her %>%
+  filter(word2 %in% include$token)
+
+herCount <- her %>%
+  count(word1,word2) %>%
   arrange(desc(n)) %>%
-  unite(bigram,word1,word2, sep = " ")
+  select(word2,n)
+#unite(bigram,word1,word2, sep = " ")
+
+wordcloud(words = herCount$word2, freq = herCount$n, min.freq = 50, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(9,"Reds")[4:9])
+
+
+  
--- his_bigrams.R
+++ his_bigrams.R
@@ -1,6 +1,8 @@
 
 library(tidyverse)
 library(tidytext)
+library(udpipe)
+library(wordcloud)
 
 his <- read_csv("hisTweets.csv") %>%
   select(screen_name, text) %>% 
@@ -8,6 +10,25 @@ his <- read_csv("hisTweets.csv") %>%
   separate(bigram, c("word1", "word2"), sep = " ") %>%
   filter(word1 == "his")
 
-his %>% count(word1,word2) %>%
+udmodel <- udpipe_download_model(language = "english")
+
+include <- udpipe(x = his$word2,
+                  object = udmodel)
+
+include <- include %>%
+  select(token,upos) %>%
+  filter(upos =="NOUN") %>%
+  select(token) 
+
+his <- his %>%
+  filter(word2 %in% include$token)
+
+hisCount <- his %>%
+  count(word1,word2) %>%
   arrange(desc(n)) %>%
-  unite(bigram,word1,word2, sep = " ")
+  select(word2,n)
+  #unite(bigram,word1,word2, sep = " ")
+
+wordcloud(words = hisCount$word2, freq = hisCount$n, min.freq = 50, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(9,"Blues")[4:9])
+
